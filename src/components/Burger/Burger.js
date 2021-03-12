e5bb74999ca171484e33a8119a4f4ffbdc0a61f2
Arsyel <arsyel@agate.id>
2021-03-12 09:35:40 +0700

    update dynamic burger render

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,14 +1,29 @@
 import Style from "./Burger.module.css";
+import PropTypes from "prop-types";
 import { BurgerIngredient } from "./BurgerIngredient/BurgerIngredient";
 
-export const Burger = () => {
+export const Burger = ({ ingredients }) => {
+	let transformedIngredients = Object.keys(ingredients).map((ingredientKey) => {
+		/** @type {number} */
+		const count = Reflect.get(ingredients, ingredientKey);
+		return [...Array(count)]
+			.map((_, i) => <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />);
+	});
+
+	const isEmptyIngredients = transformedIngredients.reduce((acc, curr) => acc.concat(curr), []).length === 0;
+	if (isEmptyIngredients) {
+		transformedIngredients = <p>Please add some ingredients here!</p>;
+	}
+
 	return (
 		<div className={Style.Burger}>
 			<BurgerIngredient type="bread-top" />
-			<BurgerIngredient type="meat" />
-			<BurgerIngredient type="cheese" />
-			<BurgerIngredient type="meat" />
+			{transformedIngredients}
 			<BurgerIngredient type="bread-bottom" />
 		</div>
 	);
+};
+
+Burger.propTypes = {
+	ingredients: PropTypes.object,
 };
