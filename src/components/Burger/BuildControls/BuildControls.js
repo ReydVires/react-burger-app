65fc07f353c3df0f249fbcdd296645c699d2679a
Arsyel <arsyel@agate.id>
2021-03-12 15:24:39 +0700

    passing disable info handler

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -3,21 +3,28 @@ import Style from "./BuildControls.module.css";
 import PropTypes from "prop-types";
 
 const controls = [
-	{ label: "Salad", type: "salad"},
-	{ label: "Meat", type: "meat"},
-	{ label: "Cheese", type: "cheese"},
-	{ label: "Bacon", type: "bacon"},
+	{ label: "Salad", type: "salad" },
+	{ label: "Meat", type: "meat" },
+	{ label: "Cheese", type: "cheese" },
+	{ label: "Bacon", type: "bacon" },
 ];
 
-export const BuildControls = ({ handleAdd, handleRemove }) => {
+export const BuildControls = ({ handleAdd, handleRemove, disabled, price }) => {
+	const handleDisable = (type) => {
+		const item = disabled.find((val) => val.type === type);
+		return item.isDisable;
+	};
+
 	return (
 		<div className={Style.BuildControls}>
+			<p>Current price: <strong>Rp{price.toFixed(2)}</strong></p>
 			{controls.map((ctrl) => (
 				<BuildControl
 					key={ctrl.type}
 					label={ctrl.label}
 					onAdd={() => handleAdd(ctrl.type)}
 					onRemove={() => handleRemove(ctrl.type)}
+					isDisable={handleDisable(ctrl.type)}
 				/>
 			))}
 		</div>
@@ -27,4 +34,6 @@ export const BuildControls = ({ handleAdd, handleRemove }) => {
 BuildControls.propTypes = {
 	handleAdd: PropTypes.func.isRequired,
 	handleRemove: PropTypes.func.isRequired,
+	disabled: PropTypes.array,
+	price: PropTypes.number,
 };
