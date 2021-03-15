d99b972b217e1cb2f78fe6d6d5b73a1de2243abd
Arsyel <arsyel@agate.id>
2021-03-15 11:28:04 +0700

    implement backdrop at modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,12 +1,17 @@
+import { Auxs } from "../../../hoc/Auxs";
+import { Backdrop } from "../Backdrop/Backdrop";
 import Style from "./Modal.module.css";
 
-export const Modal = ({ children, show }) => {
+export const Modal = ({ children, show, modalClosed }) => {
 	return (
-		<div className={Style.Modal} style={{
-			transform: show ? "translateY(0)" : "translateY(-100vh)",
-			opacity: show ? "1" : "0",
-		}}>
-			{children}
-		</div>
+		<Auxs>
+			<Backdrop show={show} onClick={modalClosed} />
+			<div className={Style.Modal} style={{
+				transform: show ? "translateY(0)" : "translateY(-100vh)",
+				opacity: show ? "1" : "0",
+			}}>
+				{children}
+			</div>
+		</Auxs>
 	);
 };
