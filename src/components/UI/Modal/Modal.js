95588c96f9e78f661c307229c95273c4ddd0f4e2
Arsyel <arsyel@agate.id>
2021-03-19 08:47:24 +0700

    add shoulComponentUpdate like for re render

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,8 +1,15 @@
+import { useEffect, useState } from "react";
 import { Auxs } from "../../../hoc/Auxs";
 import { Backdrop } from "../Backdrop/Backdrop";
 import Style from "./Modal.module.css";
 
 export const Modal = ({ children, show, modalClosed }) => {
+	const [renderChildren, setRenderChildren] = useState(false);
+
+	useEffect(() => {
+		setRenderChildren(show);
+	}, [show]);
+
 	return (
 		<Auxs>
 			<Backdrop show={show} onClick={modalClosed} />
@@ -10,7 +17,7 @@ export const Modal = ({ children, show, modalClosed }) => {
 				transform: show ? "translateY(0)" : "translateY(-100vh)",
 				opacity: show ? "1" : "0",
 			}}>
-				{children}
+				{renderChildren && children}
 			</div>
 		</Auxs>
 	);
